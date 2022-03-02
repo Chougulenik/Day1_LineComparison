13ecb3a82afdc58deada87e47794114b073ca351
Chougulenik <nikitachougule47@gmail.com>
2022-03-02 13:07:31 -0500

    added UC-2

--- LineComparison/src/com/bridgelabz/LineComparison.java
+++ LineComparison/src/com/bridgelabz/LineComparison.java
@@ -4,7 +4,7 @@ public class LineComparison {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Welcome to Line Comparsion Computation Program");
+System.out.println("Welcome to Line Comparsion Computation Program");
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter X1 & Y1 co-ordinates");
@@ -13,10 +13,29 @@ public class LineComparison {
 		System.out.println("Enter X2 & Y2 co-ordinates");
 		int X2 = sc.nextInt();
 		int Y2 = sc.nextInt();
-		//calculate the length
-		double len = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
-		System.out.println("Length of line : " +len);
+		//calculate the length1
+		double length1 = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+		
+		System.out.println("Enter X3 & Y3 co-ordinates");
+		int X3 = sc.nextInt();
+		int Y3 = sc.nextInt();
+		System.out.println("Enter X4 & Y4 co-ordinates");
+		int X4 = sc.nextInt();
+		int Y4 = sc.nextInt();
+		//calculate the length2
+		double length2 = Math.sqrt((X2-X1)*(X2-X1)+(Y2-Y1)*(Y2-Y1));
+		
+		System.out.println("Length of line : " +length1);
+		System.out.println("Length of line : " +length2);
 
+		if(length1 == length2)
+			
+			System.out.println("Both are equal");
+		
+		else
+		
+			System.out.println("Not Equal");
+			
 	}
 
 }
